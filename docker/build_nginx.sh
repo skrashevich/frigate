60a11ab964c74a4b74f6e1b45fd8c45a3fdfaab0
Sergey Krashevich <svk@svk.su>
2023-03-28 06:08:21 +0300

    Update build_nginx.sh

--- docker/build_nginx.sh
+++ docker/build_nginx.sh
@@ -4,8 +4,8 @@ set -euxo pipefail
 
 NGINX_VERSION="1.23.3"
 VOD_MODULE_VERSION="1.31"
-SECURE_TOKEN_MODULE_VERSION="1.5"
-RTMP_MODULE_VERSION="1.2.2-r1"
+SECURE_TOKEN_MODULE_VERSION="openssl3-hmac-deprecation.zip" #1.5
+RTMP_MODULE_VERSION="master" #1.2.2-r1
 
 cp /etc/apt/sources.list /etc/apt/sources.list.d/sources-src.list
 sed -i 's|deb http|deb-src http|g' /etc/apt/sources.list.d/sources-src.list
@@ -21,7 +21,7 @@ wget -nv https://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 tar -zxf nginx-${NGINX_VERSION}.tar.gz -C /tmp/nginx --strip-components=1
 rm nginx-${NGINX_VERSION}.tar.gz
 mkdir /tmp/nginx-vod-module
-wget -nv https://github.com/kaltura/nginx-vod-module/archive/refs/tags/${VOD_MODULE_VERSION}.tar.gz
+wget -nv https://github.com/comrat/nginx-vod-module/archive/refs/tags/${VOD_MODULE_VERSION}.tar.gz
 tar -zxf ${VOD_MODULE_VERSION}.tar.gz -C /tmp/nginx-vod-module --strip-components=1
 rm ${VOD_MODULE_VERSION}.tar.gz
     # Patch MAX_CLIPS to allow more clips to be added than the default 128
@@ -43,13 +43,13 @@ EOF
 
 
 mkdir /tmp/nginx-secure-token-module
-wget https://github.com/kaltura/nginx-secure-token-module/archive/refs/tags/${SECURE_TOKEN_MODULE_VERSION}.tar.gz
-tar -zxf ${SECURE_TOKEN_MODULE_VERSION}.tar.gz -C /tmp/nginx-secure-token-module --strip-components=1
-rm ${SECURE_TOKEN_MODULE_VERSION}.tar.gz
+wget https://github.com/kaltura/nginx-secure-token-module/archive/refs/heads/${SECURE_TOKEN_MODULE_VERSION}.zip
+tar -zxf ${SECURE_TOKEN_MODULE_VERSION}.zip -C /tmp/nginx-secure-token-module --strip-components=1
+rm ${SECURE_TOKEN_MODULE_VERSION}.zip
 mkdir /tmp/nginx-rtmp-module
-wget -nv https://github.com/sergey-dryabzhinsky/nginx-rtmp-module/archive/refs/tags/v${RTMP_MODULE_VERSION}.tar.gz
-tar -zxf v${RTMP_MODULE_VERSION}.tar.gz -C /tmp/nginx-rtmp-module --strip-components=1
-rm v${RTMP_MODULE_VERSION}.tar.gz
+wget -nv https://github.com/mannyamorim/nginx-rtmp-module/archive/refs/heads/${RTMP_MODULE_VERSION}.zip
+tar -zxf ${RTMP_MODULE_VERSION}.zip -C /tmp/nginx-rtmp-module --strip-components=1
+rm ${RTMP_MODULE_VERSION}.zip
 
 cd /tmp/nginx
 
