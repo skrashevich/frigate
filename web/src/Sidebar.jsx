fc5e4fb074cf91db40f8d62791cf9c797b561c24
Sergey Krashevich <svk@svk.su>
2023-05-21 21:15:15 +0300

    Add documentation build step to Dockerfile and update Documentation link in Sidebar

--- web/src/Sidebar.jsx
+++ web/src/Sidebar.jsx
@@ -58,7 +58,7 @@ export default function Sidebar() {
           <Separator />
         </Fragment>
       ) : null}
-      <Destination className="self-end" href="https://docs.frigate.video" text="Documentation" />
+      <Destination className="self-end" href="/docs/" text="Documentation" />
       <Destination className="self-end" href="https://github.com/blakeblackshear/frigate" text="GitHub" />
     </NavigationDrawer>
   );
