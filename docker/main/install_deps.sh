0ff733ebf0df6186828ab4d4a3f941b063230acb
Sergey Krashevich <svk@svk.su>
2023-07-29 05:28:59 +0300

    Remove LLVM-14 and symlink libLLVM-14.so.1 to libLLVM-15.so in install_deps.sh

--- docker/main/install_deps.sh
+++ docker/main/install_deps.sh
@@ -84,6 +84,10 @@ apt-get clean autoclean -y
 apt-get autoremove --purge -y
 rm -rf /var/lib/apt/lists/*
 
+# Remove LLVM-14
+cd $(dirname `ldconfig -p | grep libLLVM-15.so.1 | cut -d' ' -f4`) && \
+rm -f libLLVM-14.so.1  && ln -s libLLVM-15.so libLLVM-14.so.1
+
 # Install yq, for frigate-prepare and go2rtc echo source
 curl -fsSL \
     "https://github.com/mikefarah/yq/releases/download/v4.33.3/yq_linux_$(dpkg --print-architecture)" \
